cc0ae028e596cfc22b0be78d0a4647bb0e26cc5b
Fabricio <fabri0176@gmail.com>
2022-02-18 21:00:33 -0500

    Prueba lista personas pasada a la vista

--- src/main/java/com/nfp/springtest/InitController.java
+++ src/main/java/com/nfp/springtest/InitController.java
@@ -1,5 +1,6 @@
 package com.nfp.springtest;
 
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +10,10 @@ import org.springframework.ui.Model;
 @Controller
 @Slf4j
 public class InitController {
-       
+
     @Value("${index.saludo}")
     private String saludo;
-    
+
     @GetMapping("/")
     public String helloWorld(Model model) {
         String msg = "Hola mundo desde variables";
@@ -22,10 +23,22 @@ public class InitController {
         persona.setApellidos("Parra Gonzalez");
         persona.setEmail("fabri@gmail.com");
         persona.setTelefono("315421236");
+
+        var persona2 = new Persona();
+        persona2.setNombres("Katherine");
+        persona2.setApellidos("Riascos");
+        persona2.setEmail("kriascos@gmail.com");
+        persona2.setTelefono("2325365");
         
+        var personas = new ArrayList();
+        personas.add(persona);
+        personas.add(persona2);
+        
+
         model.addAttribute("msg", msg);
         model.addAttribute("saludo", saludo);
         model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 
