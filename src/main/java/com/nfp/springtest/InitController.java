38c77b28471111852f900a2a45b4619282247b7e
Fabricio <fabri0176@gmail.com>
2022-02-18 20:07:52 -0500

    Probando log

--- src/main/java/com/nfp/springtest/InitController.java
+++ src/main/java/com/nfp/springtest/InitController.java
@@ -3,11 +3,15 @@ package com.nfp.springtest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class InitController {
 	
 	@GetMapping("/")
-	public String inicio() {
-		return "Hola mundo con Spring NFP";
+	public String helloWorld() {
+		log.info("Ejecutando controlador REST");
+		return "Hola mundo con Spring NFP.";
 	}
 }
