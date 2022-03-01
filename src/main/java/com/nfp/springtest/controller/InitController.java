c8c170ab028e09ac8b96ef84fe826e4315c5cf4c
Fabricio <fabri0176@gmail.com>
2022-02-28 20:43:49 -0500

    Confgurando controlador utilizando el servicio

--- src/main/java/com/nfp/springtest/controller/InitController.java
+++ src/main/java/com/nfp/springtest/controller/InitController.java
@@ -2,6 +2,7 @@ package com.nfp.springtest.controller;
 
 
 import com.nfp.springtest.dao.IPersonDao;
+import com.nfp.springtest.service.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,12 @@ import org.springframework.ui.Model;
 public class InitController {
     
     @Autowired(required = true)  
-    private IPersonDao iPersonDao;
+    private PersonService personService;
     
     @GetMapping("/")
     public String helloWorld(Model model) {
         log.info("Ejecutando controlador SPRING MVC");
-        var persons = iPersonDao.findAll();
+        var persons = personService.personsList();
         log.info("Ejecutando controlador SPRING MVC 2");
         model.addAttribute("persons", persons);
         return "index";
