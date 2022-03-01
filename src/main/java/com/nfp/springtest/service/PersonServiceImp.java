c72c37ba2d8fe99930992d71f871e2e9f1d7eff8
Fabricio <fabri0176@gmail.com>
2022-02-28 20:41:54 -0500

    Configuracion de transaccionalidad en servicio

--- src/main/java/com/nfp/springtest/service/PersonServiceImp.java
+++ src/main/java/com/nfp/springtest/service/PersonServiceImp.java
@@ -9,34 +9,38 @@ import com.nfp.springtest.domain.Person;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Fabricio
  */
-
 @Service
 public class PersonServiceImp implements PersonService {
-    
+
     @Autowired
     private IPersonDao personDao;
-    
+
     @Override
+    @Transactional(readOnly = true)
     public List<Person> personsList() {
         return (List<Person>) personDao.findAll();
     }
 
     @Override
+    @Transactional
     public void save(Person person) {
         personDao.save(person);
     }
 
     @Override
+    @Transactional
     public void delete(Person person) {
         personDao.delete(person);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Person findPerson(Person person) {
         //Busca si no encuentra lanza nul
         return personDao.findById(person.getId()).orElse(null);
