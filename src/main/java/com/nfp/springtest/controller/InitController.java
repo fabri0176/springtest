5cac752f6decc35a9e0302748d3dd3b4c15991f9
Fabricio <fabri0176@gmail.com>
2022-03-02 21:04:10 -0500

    Person create

--- src/main/java/com/nfp/springtest/controller/InitController.java
+++ src/main/java/com/nfp/springtest/controller/InitController.java
@@ -1,21 +1,22 @@
 package com.nfp.springtest.controller;
 
-
 import com.nfp.springtest.dao.IPersonDao;
+import com.nfp.springtest.domain.Person;
 import com.nfp.springtest.service.PersonService;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class InitController {
-    
-    @Autowired(required = true)  
+
+    @Autowired(required = true)
     private PersonService personService;
-    
+
     @GetMapping("/")
     public String helloWorld(Model model) {
         log.info("Ejecutando controlador SPRING MVC");
@@ -23,6 +24,19 @@ public class InitController {
         log.info("Ejecutando controlador SPRING MVC 2");
         model.addAttribute("persons", persons);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String add(Person person) {
+
+        return "personEdit";
+    }
+
+    @PostMapping("/save")
+    public String save(Person person) {
+        personService.save(person);
+
+        return "redirect:/";
     }
 
 }
